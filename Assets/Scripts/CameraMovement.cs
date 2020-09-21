553e3692cd43599ed484e63f7bd26beea9527dfe
AsuraAria <48623275+AsuraAria@users.noreply.github.com>
2020-09-21 18:12:07 +0200

    Part transition

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,7 +6,8 @@ public class CameraMovement : MonoBehaviour
 {
     public Transform target;
     public float smoothing;
-
+    public Vector2 minPosition;
+    public Vector2 maxPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,8 @@ public class CameraMovement : MonoBehaviour
         if(transform.position != target.position)
         {
             Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
             transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
         }
     }
